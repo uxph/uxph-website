297369d18113e4185a0a77c9740d6a771115eebb
mjarenyap <mjaren.yap@gmail.com>
2020-11-28 18:43:52 +0800

    Made FAQ categories and installed reactstrap for the modal

---
+++ src/helpers/faq_categories.js
@@ -0,0 +1,36 @@
+const categories = [
+  {
+    label: "All topics",
+    value: "all",
+  },
+  {
+    label: "Events & Certificates",
+    value: "events",
+  },
+  {
+    label: "Community and Volunteering",
+    value: "internals",
+  },
+  {
+    label: "Speakers, Partnerships and Sponsorships",
+    value: "externals",
+  },
+  {
+    label: "Conferences",
+    value: "conferences",
+  },
+  {
+    label: "Ticketing & Purchases",
+    value: "ticketing",
+  },
+  {
+    label: "Reporting an incident",
+    value: "incident",
+  },
+  {
+    label: "Code of Conduct",
+    value: "coc",
+  },
+];
+
+export default categories;
