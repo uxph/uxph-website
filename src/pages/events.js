5a77139d26bfded33515ef530b2655b9a54d2f7f
mjarenyap <mjaren.yap@gmail.com>
2020-12-19 17:54:32 +0800

    Refined info structure of the events sidebar

--- src/pages/events.js
+++ src/pages/events.js
@@ -3,13 +3,11 @@ import { Layout, SEO, HostEventItem } from "../components/Components";
 import { Container, Box, Grid } from "@material-ui/core";
 
 const EventsPage = () => {
-  const hostFeature = [
-    {
-      heading: "Host your own UXPH event (Coming Soon!)",
-      description:
-        "Have an event idea? You can host your own UXPH-sanctioned event!",
-    },
-  ];
+  const hostFeature = {
+    heading: "Host your own UXPH event (Coming Soon!)",
+    description:
+      "Have an event idea? You can host your own UXPH-sanctioned event!",
+  };
   return (
     <Layout active="/events">
       <SEO title="Events" />
@@ -17,13 +15,12 @@ const EventsPage = () => {
       <Box component="section" py={12}>
         <Container maxWidth="md">
           <Grid container spacing={4}>
-            {hostFeature.map((host, index) => {
-              return (
-                <Grid item md={5} key={index}>
-                  <HostEventItem host={host} />
-                </Grid>
-              );
-            })}
+            <Grid item md={7}>
+              <h1>List of events here</h1>
+            </Grid>
+            <Grid item md={5}>
+              <HostEventItem host={hostFeature} />
+            </Grid>
           </Grid>
         </Container>
       </Box>
