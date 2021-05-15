dbdce1bf2e76e9919e273696d0642537a3794353
Gavin Raine Dizon <60138634+gavindizon@users.noreply.github.com>
2021-05-15 10:27:24 +0800

    Added events images, blog page, and changed header design

--- src/components/BlogItem/BlogItem.js
+++ src/components/BlogItem/BlogItem.js
@@ -4,55 +4,57 @@ import styles from "./BlogItem.module.scss";
 import classNames from "classnames";
 import { useMediaQuery } from "react-responsive";
 
-const BlogItem = (blog) => {
-  const isMaxWidth785 = useMediaQuery({ query: "(max-width: 784px)" });
-  const isMaxWidth600 = useMediaQuery({ query: "(max-width: 599px)" });
-  return (
-    <Box component="div" p={2} className={styles["blog_item"]}>
-      <Grid container spacing={isMaxWidth600 ? 2 : 4} alignItems="center">
-        <Grid item md={5} sm={isMaxWidth785 ? 6 : 5}>
-          <img
-            src="https://miro.medium.com/max/700/0*B_owcQpugXNQbbVu"
-            alt="Ready, Set, Research! 3 Things You Need to Know as a Fresh UX
-            Researcher"
-            className={classNames(styles["featured_image"], {
-              [`${styles["mobile"]}`]: isMaxWidth600,
-            })}
-          />
-        </Grid>
-        <Grid item md={7} sm={6}>
-          <h3 className="margin-bottom-16 text-dark">
-            Ready, Set, Research! 3 Things You Need to Know as a Fresh UX
-            Researcher
-          </h3>
-          {!isMaxWidth600 && (
-            <p
-              className={classNames(
-                "text-gray margin-bottom-16",
-                styles["blog_description"]
-              )}
-            >
-              As a Filipino student taking my first steps into the field of UX
-              Research, figuring out where to start has always been the most
-              difficult part. However, thanks to UXR Conference...
-            </p>
-          )}
+const BlogItem = ({ blog }) => {
+    const isMaxWidth785 = useMediaQuery({ query: "(max-width: 784px)" });
+    const isMaxWidth600 = useMediaQuery({ query: "(max-width: 599px)" });
 
-          <Box component="div" className={styles["blog_author"]}>
-            <img
-              src="https://miro.medium.com/max/700/0*B_owcQpugXNQbbVu"
-              alt="Pauline Baterna"
-              className={styles["profile_photo"]}
-            />
-            <Box component="div">
-              <h4 className="text-dark font-weight-medium">Pauline Baterna</h4>
-              <small className="text-gray">July 15, 2020</small>
-            </Box>
-          </Box>
-        </Grid>
-      </Grid>
-    </Box>
-  );
+    if (!blog) {
+        blog = {
+            name: "Ready, Set, Research! 3 Things You Need to Know as a Fresh UX Researcher",
+            date: "July 15, 2020",
+            description:
+                "This will be my first time attending a UX+ Conference. I'm excited to learn from everyone's journey and their topics!",
+            image_square_url: "https://miro.medium.com/max/700/0*B_owcQpugXNQbbVu",
+            image_banner_url: "https://miro.medium.com/max/700/0*B_owcQpugXNQbbVu",
+            become_speaker_url: "#",
+            blog_url: "/blog/attending-asias-largest-ux-event-as-a-scholar",
+            livestream_url: "#",
+            blog_id: 7,
+            tag: "UX+ Conference",
+        };
+    }
+
+    return (
+        <Box component="div" p={2} className={styles["blog_item"]}>
+            <Grid container spacing={isMaxWidth600 ? 2 : 4} alignItems="center">
+                <Grid item md={5} sm={isMaxWidth785 ? 6 : 5}>
+                    <img
+                        src={blog.image_square_url}
+                        alt={blog.name}
+                        className={classNames(styles["featured_image"], {
+                            [`${styles["mobile"]}`]: isMaxWidth600,
+                        })}
+                    />
+                </Grid>
+                <Grid item md={7} sm={6}>
+                    <h3 className="margin-bottom-16 text-dark">{blog.name}</h3>
+                    {!isMaxWidth600 && (
+                        <p className={classNames("text-gray margin-bottom-16", styles["blog_description"])}>
+                            {blog.description}
+                        </p>
+                    )}
+
+                    <Box component="div" className={styles["blog_author"]}>
+                        <img src={blog.image_square_url} alt="Pauline Baterna" className={styles["profile_photo"]} />
+                        <Box component="div">
+                            <h4 className="text-dark font-weight-medium">Pauline Baterna</h4>
+                            <small className="text-gray">{blog.date}</small>
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid>
+        </Box>
+    );
 };
 
 export default BlogItem;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,20 +2,21 @@ import React from "react";
 import { Box, Container } from "@material-ui/core";
 
 const Header = ({ children, bgImage }) => {
-  return (
-    <Box
-      component="section"
-      py={12}
-      className="bg-dark-blue"
-      style={{
-        background: `url(${bgImage})`,
-        backgroundAttachment: "fixed",
-        backgroundSize: "cover",
-      }}
-    >
-      <Container maxWidth="md">{children}</Container>
-    </Box>
-  );
+    return (
+        <Box
+            component="section"
+            py={12}
+            //            className="bg-dark-blue"
+            style={{
+                background: `url(${bgImage}),rgba(0,0,0,0.72)`,
+                backgroundAttachment: "fixed",
+                backgroundSize: "cover",
+                backgroundBlendMode: "multiply",
+            }}
+        >
+            <Container maxWidth="md">{children}</Container>
+        </Box>
+    );
 };
 
 export default Header;
