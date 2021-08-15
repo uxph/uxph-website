ffd02a3ffc01f73b6c6de790bcfaf2e304f0885f
Gavin Raine Dizon <60138634+gavindizon@users.noreply.github.com>
2021-08-15 20:26:25 +0800

    Added changes from the discussion last Aug 13

--- src/components/BlogItem/BlogItem.js
+++ src/components/BlogItem/BlogItem.js
@@ -2,53 +2,50 @@ import React from "react";
 import { Box } from "@material-ui/core";
 import { Link } from "gatsby";
 import { Button } from "../Components";
+import dateFormat from "dateformat";
 import { NavigateNextRounded } from "@material-ui/icons";
 
 const BlogItem = ({ blog }) => {
-  const { date, slug, cover, title } = blog.node.frontmatter;
-  const description = blog.node.html.replace(/(<([^>]+)>)/gi, "");
-  return (
-    <Link to="/">
-      <Box p={1} borderRadius={8} className="blog_item">
-        <Box
-          component="div"
-          display="block"
-          height={200}
-          borderRadius={4}
-          className="bg-light"
-          overflow="hidden"
-          mb={2}
-        >
-          <img src={cover} className="featured_image" alt={title} />
-        </Box>
-        <Box p={1}>
-          <h3 className="margin-bottom-8 font-size-18 line-height-24 blog_title">
-            {title}
-          </h3>
-          <p className="text-gray margin-bottom-16 blog_description font-size-14 line-height-24 two-liner">
-            {description}
-          </p>
-          <Box component="div" className="blog_author" mb={4}>
-            <img
-              src="https://miro.medium.com/max/700/0*B_owcQpugXNQbbVu"
-              alt="Pauline Baterna"
-              className="profile_photo"
-            />
-            <Box component="div">
-              <h4 className="text-primary font-size-14 font-weight-normal">
-                Pauline Baterna
-              </h4>
-              <small className="text-gray">{date}</small>
+    const { date, slug, cover, title } = blog.node.frontmatter;
+    const description = blog.node.html.replace(/(<([^>]+)>)/gi, "");
+    return (
+        <Link to={slug}>
+            <Box p={1} borderRadius={8} className="blog_item">
+                <Box
+                    component="div"
+                    display="block"
+                    height={200}
+                    borderRadius={4}
+                    className="bg-light"
+                    overflow="hidden"
+                    mb={2}
+                >
+                    <img src={cover} className="featured_image" alt={title} />
+                </Box>
+                <Box p={1}>
+                    <h3 className="margin-bottom-8 font-size-18 line-height-24 blog_title">{title}</h3>
+                    <p className="text-gray margin-bottom-16 blog_description font-size-14 line-height-24 two-liner">
+                        {description}
+                    </p>
+                    <Box component="div" className="blog_author" mb={4}>
+                        <img
+                            src="https://miro.medium.com/max/700/0*B_owcQpugXNQbbVu"
+                            alt="Pauline Baterna"
+                            className="profile_photo"
+                        />
+                        <Box component="div">
+                            <h4 className="text-primary font-size-14 font-weight-normal">Pauline Baterna</h4>
+                            <small className="text-gray">{dateFormat(date, "mmm d, yyyy")}</small>
+                        </Box>
+                    </Box>
+                    <Button variant="link" href={slug} className="blog_cta">
+                        <span className="text-uppercase">Read more</span>
+                        <NavigateNextRounded />
+                    </Button>
+                </Box>
             </Box>
-          </Box>
-          <Button variant="link" href={slug} className="blog_cta">
-            <span className="text-uppercase">Read more</span>
-            <NavigateNextRounded />
-          </Button>
-        </Box>
-      </Box>
-    </Link>
-  );
+        </Link>
+    );
 };
 
 export default BlogItem;
--- src/pages/events.js
+++ src/pages/events.js
@@ -1,100 +1,87 @@
 import React from "react";
-import {
-  Layout,
-  SEO,
-  Card,
-  EventItem,
-  Header,
-  Button,
-} from "../components/Components";
+import { Layout, SEO, Card, EventItem, Header, Button } from "../components/Components";
+import { graphql } from "gatsby";
 import { Container, Box, Grid } from "@material-ui/core";
 
-const EventsPage = ({data}) => {
+const EventsPage = ({ data }) => {
+    // const hostFeature = {
+    //   heading: "Host your own UXPH event (Coming Soon!)",
+    //   description:
+    //     "Have an event idea? You can host your own UXPH-sanctioned event!",
+    // };
 
-  // const hostFeature = {
-  //   heading: "Host your own UXPH event (Coming Soon!)",
-  //   description:
-  //     "Have an event idea? You can host your own UXPH-sanctioned event!",
-  // };
+    const events = data.allMarkdownRemark.edges;
+    const featuredEvent = events[0].node.frontmatter;
+    return (
+        <Layout active="/events">
+            <SEO title="Events" />
 
-  const events = data.allMarkdownRemark.edges;
-  console.log (events);
-  const featuredEvent = events[0].node.frontmatter;
-console.log(featuredEvent);
-  return (
-    <Layout active="/events">
-      <SEO title="Events" />
+            <Header relative={true} pb={19}>
+                <h1 className="margin-bottom-24 text-white">Featured Event</h1>
+                <Card suspend={300} p={1}>
+                    <Grid container spacing={4} alignItems="center">
+                        <Grid item md={7}>
+                            <Box p={2}>
+                                <Box
+                                    component="small"
+                                    fontSize={14}
+                                    display="block"
+                                    className="text-uppercase text-primary"
+                                    mb={2}
+                                >
+                                    {featuredEvent.date} • {featuredEvent.venue}
+                                </Box>
+                                <h2 className="font-size-24 line-height-32 two-liner">{featuredEvent.name}</h2>
+                                <p className="two-liner font-size-16 margin-bottom-16">{featuredEvent.description}</p>
+                                <Button href="/">
+                                    <span className="font-size-14">View event details</span>
+                                </Button>
+                            </Box>
+                        </Grid>
+                        <Grid item md={5}>
+                            <Box borderRadius={4} height={250} className="bg-light"></Box>
+                        </Grid>
+                    </Grid>
+                </Card>
+            </Header>
 
-      <Header relative={true} pb={19}>
-        <h1 className="margin-bottom-24 text-white">Featured Event</h1>
-        <Card suspend={300} p={1}>
-          <Grid container spacing={4} alignItems="center">
-            <Grid item md={7}>
-              <Box p={2}>
-                <Box
-                  component="small"
-                  fontSize={14}
-                  display="block"
-                  className="text-uppercase text-primary"
-                  mb={2}
-                >
-                  {featuredEvent.date} • {featuredEvent.venue}
-                </Box>
-                <h2 className="font-size-24 line-height-32 two-liner">
-                  {featuredEvent.name}
-                </h2>
-                <p className="two-liner font-size-16 margin-bottom-16">
-                  {featuredEvent.description}
-                </p>
-                <Button href="/">
-                  <span className="font-size-14">View event details</span>
-                </Button>
-              </Box>
-            </Grid>
-            <Grid item md={5}>
-              <Box borderRadius={4} height={250} className="bg-light"></Box>
-            </Grid>
-          </Grid>
-        </Card>
-      </Header>
-
-      <Box component="section" pt={26} pb={12} className="bg-light-1">
-        <Container maxWidth="lg">
-          <h2 className="margin-bottom-32 font-size-40">Past Events</h2>
-          <Grid container spacing={3}>
-            {events.map((event, index) => {
-              return index !== 0 ? (
-                <Grid item md={4} key={index}>
-                  <EventItem event={event} />
-                </Grid>
-              ) : null;
-            })}
-          </Grid>
-        </Container>
-      </Box>
-    </Layout>
-  );
+            <Box component="section" pt={26} pb={12} className="bg-light-1">
+                <Container maxWidth="lg">
+                    <h2 className="margin-bottom-32 font-size-40">Past Events</h2>
+                    <Grid container spacing={3}>
+                        {events.map((event, index) => {
+                            return index !== 0 ? (
+                                <Grid item md={4} key={index}>
+                                    <EventItem event={event} />
+                                </Grid>
+                            ) : null;
+                        })}
+                    </Grid>
+                </Container>
+            </Box>
+        </Layout>
+    );
 };
 
 export const dataquery = graphql`
-query EventsQuery {
-  allMarkdownRemark(filter: {frontmatter: {type: {eq: "event"}}}) {
-    edges {
-      node {
-        frontmatter {
-          slug
-          title
-          cover
-          date
-          type
-          time
-          venue
+    query EventsQuery {
+        allMarkdownRemark(filter: { frontmatter: { type: { eq: "event" } } }) {
+            edges {
+                node {
+                    frontmatter {
+                        slug
+                        title
+                        cover
+                        date
+                        type
+                        time
+                        venue
+                    }
+                    html
+                }
+            }
         }
-        html
-      }
     }
-  }
-}
 `;
 
 export default EventsPage;
